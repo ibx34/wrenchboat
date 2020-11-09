47d1ea8ea481499a7ca7a121ac6c5307ac0219a0
ibx34 <51100404+ibx34@users.noreply.github.com>
2020-11-09 11:19:00 -0800

    Updated userinfo, add Serverinfo

--- wrenchboat/utility.py
+++ wrenchboat/utility.py
@@ -21,35 +21,38 @@ class utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(
-        name="gibmeinfo",
-        usage="@user",
-        description="Get info on a user. (Don't add a user to get your own info).",
-    )
+    @commands.command(name="gibmeinfo",usage="@user",description="Get info on a user. (Don't add a user to get your own info).",)
     async def _userinfo(self, ctx, user: discord.Member = None):
         if user is None:
             user = ctx.author
 
-        try:
-            roles = [x.mention for x in user.roles]
-            await ctx.channel.send(
-                dedent(
-                    f"""
-            **User**: {user} ({user.id}) ({user.mention})
-            **Create At**: {user.created_at.strftime('%m/%d/%Y')} ({arrow.get(user.created_at).humanize()})
-            **Joined At**: {user.joined_at.strftime('%m/%d/%Y')} ({arrow.get(user.joined_at).humanize()})
-            {f"**In voice**: #{user.voice.channel.name} ({user.voice.channel.id})" if user.voice else " "}
-            **Roles**: {', '.join(roles[:40])}
-            """
-                ),
-                allowed_mentions=discord.AllowedMentions(
-                    everyone=False, roles=False, users=False
-                ),
-            )
+        embed = discord.Embed(color=0x99AAB5)
+        embed.set_author(name=user,icon_url=user.avatar_url)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name="User Information", value=f"Id: {user.id}\nProfile: {user.mention}\nCreated: {arrow.get(user.created_at).humanize()} ({user.created_at})",inline=False)
+        embed.add_field(name="Member Information", value=f"Joined: {arrow.get(user.joined_at).humanize()} ({user.joined_at})",inline=False)
+        embed.add_field(name="Infractions", value=f"Total Infractions: {await self.bot.pool.fetchval('SELECT COUNT(*) FROM infractions WHERE guild = $1 AND target = $2',ctx.channel.guild.id,user.id)}\nUnique Servers: {await self.bot.pool.fetchval('SELECT COUNT(DISTINCT guild) FROM infractions WHERE target = $1',user.id)}")
+
+        try:    
+            await ctx.channel.send(embed=embed)
         except Exception as err:
-            return await ctx.channel.send(
-                f"Don't expect me to know what happened >:)\n{err}"
-            )
+            return await ctx.channel.send(f"Don't expect me to know what happened >:)\n{err}")
+
+    @commands.command(name="serverinfo",usage="None",description="Get info on your current server.")
+    async def _serverinfo(self,ctx):
+
+        embed = discord.Embed(color=0x99AAB5)
+        embed.set_author(name=ctx.guild.name,icon_url=ctx.guild.icon_url)
+        embed.set_thumbnail(url=ctx.guild.icon_url)        
+        embed.add_field(name="Server Information",value=f"Id: {ctx.channel.guild.id}\nMembers: {ctx.channel.guild.member_count:,d}\nCreated: {arrow.get(ctx.channel.guild.created_at).humanize()} ({ctx.channel.guild.created_at})",inline=False)
+        embed.add_field(name="Owner Information",value=f"Id: {ctx.channel.guild.owner.id}\nProfile: {ctx.channel.guild.owner.mention}\nCreated: {arrow.get(ctx.channel.guild.owner.created_at).humanize()} ({ctx.channel.guild.owner.created_at})",inline=False)
+        embed.add_field(name="Infractions",value=f"Total Infractions: {await self.bot.pool.fetchval('SELECT COUNT(*) FROM infractions WHERE guild = $1',ctx.channel.guild.id)}",inline=False)
+        
+        try:    
+            await ctx.channel.send(embed=embed)
+        except Exception as err:
+            return await ctx.channel.send(f"Don't expect me to know what happened >:)\n{err}")
+
 
     @commands.command(
         name="avy",
@@ -69,6 +72,15 @@ class utility(commands.Cog):
                 f"Don't expect me to know what happened >:)\n{err}"
             )
 
+    @commands.command(name="bug", usage="None", description="Found a bug? Report it at the link provided.")
+    async def _bug(self,ctx):
+
+        return await ctx.channel.send("You can report a bug here: --> <https://rb.gy/h2kktv>! Thank you in advance")
+    
+    @commands.command(name="support",usage="None",description="Need help? Join our support server and we will try our best")
+    async def _support(self,ctx):
+
+        return await ctx.channel.send(f"You can join our support server @: {config.support}")
 
 def setup(bot):
     bot.add_cog(utility(bot))
