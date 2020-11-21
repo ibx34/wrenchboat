07d67dde58c90df099be2d81224f54ec8905dfaf
ibx34 <Guamjust@gmail.com>
2020-11-20 20:38:22 -0800

    BIG UPDATE to modlogs

--- wrenchboat/utils/modlogs.py
+++ wrenchboat/utils/modlogs.py
@@ -118,66 +118,89 @@ modlogs_messages = {
 }
 
 
-async def modlogs_store(
-    self, moderator, user, reason, type, time, id, modlogs, role=None
+class modlog_related:
+    def __init__(
+        self,
+        user: discord.Member,
+        moderator: discord.Member,
+        reason: str,
+        id: int,
+        time,
+        type: str,
+        modlogs: int,
+        pool,
+        role: discord.Role = None,
+    ):
+        self.pool = pool
+        self.user = user
+        self.moderator = moderator
+        self.reason = reason
+        self.id = id
+        self.time = time
+        self.type = type
+        self.modlogs = modlogs
+        self.role = role
+
+    async def insert(self):
+        async with self.pool.acquire() as conn:
+            try:
+                await conn.execute(
+                    "INSERT INTO infractions(target,moderator,reason,id,time_punsihed,type,modlogs) VALUES($1,$2,$3,$4,$5,$6,$7)",
+                    self.user.id,
+                    self.moderator.id,
+                    self.reason,
+                    self.id,
+                    self.time,
+                    self.type,
+                    self.modlogs,
+                )
+            except Exception as err:
+                print(err)
+        return self
+
+
+async def modlogs(
+    self,
+    moderator: discord.Member,
+    user: discord.Member,
+    reason: str,
+    type: str,
+    case: int,
+    time,
+    role: discord.Role = None,
 ):
-    async with self.pool.acquire() as conn:
-        try:
-            await conn.execute(
-                "INSERT INTO infractions(guild,type,moderator,target,time_punsihed,reason,modlogs,id) VALUES($1,$2,$3,$4,$5,$6,$7,$8)",
-                user.guild.id,
-                type,
-                moderator.id,
-                user.id,
-                time,
-                reason,
-                modlogs.id,
-                self.cases[user.guild.id],
-            )
-        except Exception as err:
-            print(err)
-            traceback.print_tb(err.__traceback__)
-            return False
-        return True
 
-async def modlogs(self, moderator, user, reason, case, type, time, role=None):
-    if role is None:
-        role = user.guild.default_role
-
+    role = user.guild.default_role if role is None else role
     self.cases[user.guild.id] += 1
-    if reason == config.default_reason:
-        reason = f"No reason provided. You can add a reason with `case {self.cases[user.guild.id]} <reason>`."
-
     if self.modlog_channel.get(user.guild.id):
         try:
-            modlogs = self.get_channel(self.modlog_channel.get(user.guild.id))
-            modlogs_message = await modlogs.send(
+            channel = self.get_channel(self.modlog_channel.get(user.guild.id))
+            modlogs_message = await channel.send(
                 modlogs_messages[type]["message"].format(
-                    **{
-                        "user": user,
-                        "user_id": user.id,
-                        "user_mention": user.mention,
-                        "reason": reason,
-                        "moderator": moderator,
-                        "case": self.cases[user.guild.id],
-                        "role_id": role.id,
-                        "role": role.name,
-                    }
+                    user=user,
+                    user_id=user.id,
+                    user_mention=user.mention,
+                    reason=reason,
+                    moderator=moderator,
+                    case=self.cases[user.guild.id],
+                    role_id=role.id,
+                    role=role.name,
                 )
             )
-
         except Exception as err:
             print(err)
-            traceback.print_tb(err.__traceback__)
-    await modlogs_store(
-        self=self,
-        moderator=moderator,
+
+    modlogs = modlog_related(
+        pool=self.pool,
         user=user,
-        reason=reason,
+        moderator=moderator,
         type=type,
+        id=self.cases[user.guild.id],
         time=time,
         role=role,
-        id=self.cases[user.guild.id],
-        modlogs=modlogs_message if modlogs_message is not None else None,
+        modlogs=modlogs_message.id if modlogs_message is not None else None,
+        reason=f"No reason provided. You can add a reason with `case {self.cases[user.guild.id]} <reason>`."
+        if reason == config.default_reason
+        else reason,
     )
-
+    await modlogs.insert()
