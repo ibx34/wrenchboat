4cf046d19cb1fadcef20e31eebb90f9a233e9769
ibx34 <Guamjust@gmail.com>
2020-11-27 17:06:50 -0800

    Update config example

---
+++ config.example.py
@@ -0,0 +1,31 @@
+token = "bot token"
+prefix = ["!"] # Bot's prefixes
+guild = 123456789 # Home guild
+devs = [123456789] # Developers. Ignores cooldowns
+default_reason = "No reason provided. You can add a reason with `case <id> <reason>`." # Reason all mod commands should default to
+characters = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/'] # Characters for anti hoist or dehoist
+clean_names = ['I like attention', 'Attention Goblin', 'Specail Char User', 'Power Squad', 'Nice person :)', 'How are you today?', 'GG EZ', 'Minecraft is good', 'Hoisters are fun', 'Mods ara have stinky'] # Names used in anti hoist
+support = "" # Your support server's invite
+guild_whitelist = [] # Whitelist your guild with this.
+
+extensions = [
+    "jishaku",
+    "wrenchboat.moderation",
+    "wrenchboat.help",
+    "wrenchboat.admin",
+    "wrenchboat.utility",
+    "wrenchboat.settings",
+    "wrenchboat.images",
+    # "wrenchboat.automod", # Leave disabled till updated.
+    "wrenchboat.error",
+    "wrenchboat.tags",
+    "wrenchboat.highlight",
+    "wrenchboat.listeners",
+    "wrenchboat.logging"
+]
+db = {
+    "host": "host",
+    "database": "database",
+    "user": "user",
+    "password": "password",
+}
