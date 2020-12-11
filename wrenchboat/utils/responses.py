3b458f433cc470f2053c9a0227990cd697cb17f7
ibx34 <Guamjust@gmail.com>
2020-12-10 19:38:07 -0800

    Fixes problem when getting guild's type

--- wrenchboat/utils/responses.py
+++ wrenchboat/utils/responses.py
@@ -1,16 +1,14 @@
 import discord
 from discord.ext import commands
 
-responses = {
-    "in_channel_responses": {
-        "normal": {"en": "[{guild}] {user} has been {action}."},
-        "compact": {"en": "{user} has been {action}."},
-        "advanced": {
-            "en": "**{guild}** | {moderator} {action} {user} for {time} ({reason})."
-        },
-        "simple": {"en": "Done."},
-        "even_simpler": ":ok_hand:",
-    }
+in_line_responses = {
+    "normal": {"en": "[{guild}] {user} has been {action}."},
+    "compact": {"en": "{user} has been {action}."},
+    "advanced": {
+        "en": "**{guild}** | {moderator} {action} {user} for {time} ({reason})."
+    },
+    "simple": {"en": "Done."},
+    "even_simpler": ":ok_hand:",
 }
 
 
@@ -18,16 +16,12 @@ class Responder:
     def __init__(self, bot):
         self.bot = bot
 
-    async def in_line(
-        self, ctx, user, guild, action: str, reason, respone, time="Indefinite"
-    ):
+    @classmethod
+    async def in_line(cls, ctx, user, action: str, reason, time="Indefinite"):
         # En is currently the only language >:)
-        response = responses["in_channel_responses"][
-            self.bot.guild_responses(guild.id)
-        ]["en"]
-        await ctx.send(
-            response.format(
-                guild=guild,
+        returned_message = in_line_responses[ctx.bot.guild_responses[ctx.guild.id]]["en"]
+        await ctx.channel.send(returned_message.format(
+                guild=ctx.guild,
                 moderator=ctx.author,
                 action=action + "ed",
                 reason=reason,
