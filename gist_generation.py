953603f9d73e7dade3568253bbc6b7f0bf0df87a
ibx34 <Guamjust@gmail.com>
2020-12-12 10:27:31 -0800

    Added gist generation for token remover

---
+++ gist_generation.py
@@ -0,0 +1,38 @@
+import json
+import random
+import string
+from datetime import datetime
+import requests
+import config
+from logger import logging
+
+
+async def gist_invalidation(token, user, guild, channel, message_content):
+    logging.info(f"Bot started creating gist for token: {token}")
+    id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+    try:
+        GITHUB_API = "https://api.github.com"
+        API_TOKEN = config.github_api_token
+
+        url = GITHUB_API + "/gists"
+
+        headers = {"Authorization": "token %s" % API_TOKEN}
+        params = {"scope": "gist"}
+        payload = {
+            "description": f"Token Invalidation",
+            "public": True,
+            "files": {
+                f"Token Invalidation - {id}": {
+                    "content": f"{token}"
+                }
+            },
+        }
+        res = requests.post(url, headers=headers, params=params, data=json.dumps(payload))
+
+        j = json.loads(res.text)
+        logging.info(f"Bot finished creating gist. Link: https://gist.github.com/ibx34/{j['id']}")
+        data = {"url": f"https://gist.github.com/wrenchboat/{j['id']}","id": id}
+        return data
+
+    except Exception as err:
+        logging.fail(f"Error occured while creating gist. {err}")
