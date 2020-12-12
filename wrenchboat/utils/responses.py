27211069545f410f37706d9af8bdd1a2f204dd22
ibx34 <Guamjust@gmail.com>
2020-12-11 16:06:56 -0800

    Added more langauges

--- wrenchboat/utils/responses.py
+++ wrenchboat/utils/responses.py
@@ -2,12 +2,26 @@ import discord
 from discord.ext import commands
 
 in_line_responses = {
-    "normal": {"en": "[{guild}] {user} has been {action}."},
-    "compact": {"en": "{user} has been {action}."},
-    "advanced": {
-        "en": "**{guild}** | {moderator} {action} {user} for {time} ({reason})."
+    "normal": {
+        "en": "[{guild}] {user} has been {action}.",
+        "es": "[{guild}] {user} tiene estado {action}.",
+        "la": "[{guild}] {user} habet fuit {action}."
     },
-    "simple": {"en": "Done."},
+    "compact": {
+        "en": "{user} has been {action}.",
+        "es": "{user} tiene estado {action}.",
+        "la": "{user} habet fuit {action}."
+    },
+    "advanced": {
+        "en": "**{guild}** | {moderator} {action} {user} for {time} ({reason}).",
+        "es": "**{guild}** | {moderator} {action} {user} para {time} ({reason}).",
+        "la": "**{guild}** | {moderator} {action} {user} quia {time} ({reason})."
+    },
+    "simple": {
+        "en": "Done.",
+        "es": "Hecho.",
+        "la": "Factum.",
+    },
     "even_simpler": ":ok_hand:",
 }
 
@@ -19,7 +33,7 @@ class Responder:
     @classmethod
     async def in_line(cls, ctx, user, action: str, reason, time="Indefinite"):
         # En is currently the only language >:)
-        returned_message = in_line_responses[ctx.bot.guild_responses[ctx.guild.id]]["en"]
+        returned_message = in_line_responses[ctx.bot.guild_responses[ctx.guild.id]][ctx.bot.language[ctx.channel.guild.id]]
         await ctx.channel.send(returned_message.format(
                 guild=ctx.guild,
                 moderator=ctx.author,
